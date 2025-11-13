a815f691378cc7ef1cdf3a864816a13504348fed
adi-sen <adi-sen@users.noreply.github.com>
2025-11-13 14:57:35 -0500

    ci: update build config to make app smaller in size

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -7,6 +7,9 @@ cargo build --release -p ffi
 cbindgen --crate ffi --output src/swift/ffi.h
 swift build -c release --jobs 8
 
+strip -x .build/release/Summon
+strip -x target/release/libffi.dylib
+
 mkdir -p .build/macos/lib
 cp target/release/libffi.dylib .build/macos/lib/
 cp .build/release/Summon .build/macos/Summon
