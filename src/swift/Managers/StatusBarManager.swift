97228d65d99b7cff0011d23b908b5ec5c7ce576b
adi-sen <adi-sen@users.noreply.github.com>
2025-11-14 10:48:47 -0500

    feat: add more functionality to status bar * feat: implement clear clipboard history * feat: add extensions as a status item

--- src/swift/Managers/StatusBarManager.swift
+++ src/swift/Managers/StatusBarManager.swift
@@ -4,13 +4,30 @@ class StatusBarManager {
 	private var statusItem: NSStatusItem?
 	var onToggle: (() -> Void)?
 	var onSettings: (() -> Void)?
+	var onExtensions: (() -> Void)?
+	var onClearClipboard: (() -> Void)?
 	var onQuit: (() -> Void)?
 
 	func setup() {
 		statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
 
 		if let button = statusItem?.button {
-			button.image = NSImage(systemSymbolName: "command.circle", accessibilityDescription: "Launcher")
+			if let resourcePath = Bundle.main.resourcePath,
+			   let icon = NSImage(contentsOfFile: resourcePath + "/AppIcon.icns") {
+				let resizedIcon = NSImage(size: NSSize(width: 18, height: 18))
+				resizedIcon.lockFocus()
+				icon.draw(in: NSRect(x: 0, y: 0, width: 18, height: 18))
+				resizedIcon.unlockFocus()
+				button.image = resizedIcon
+			} else if let icon = NSImage(named: "AppIcon") {
+				let resizedIcon = NSImage(size: NSSize(width: 18, height: 18))
+				resizedIcon.lockFocus()
+				icon.draw(in: NSRect(x: 0, y: 0, width: 18, height: 18))
+				resizedIcon.unlockFocus()
+				button.image = resizedIcon
+			} else {
+				button.image = NSImage(systemSymbolName: "app.badge", accessibilityDescription: "Summon")
+			}
 			button.action = #selector(statusBarButtonClicked)
 			button.target = self
 		}
@@ -21,33 +38,36 @@ class StatusBarManager {
 	private func setupMenu() {
 		let menu = NSMenu()
 
-		let settings = AppSettings.shared
-		let shortcut = settings.launcherShortcut
-		let shortcutDisplay = shortcut.displayString
+		let aboutItem = NSMenuItem(title: "About Summon", action: #selector(showAbout), keyEquivalent: "")
+		aboutItem.target = self
 
-		let showItem = NSMenuItem(title: "Toggle Summon", action: #selector(toggleLauncher), keyEquivalent: "")
+		let updateItem = NSMenuItem(title: "Check for Updates...", action: #selector(checkUpdates), keyEquivalent: "")
+		updateItem.target = self
+
+		let showItem = NSMenuItem(title: "Toggle Launcher", action: #selector(toggleLauncher), keyEquivalent: "")
 		showItem.target = self
 
-		let paragraphStyle = NSMutableParagraphStyle()
-		paragraphStyle.tabStops = [NSTextTab(textAlignment: .right, location: 200)]
-
-		let attributedTitle = NSMutableAttributedString(string: "Toggle Summon\t\(shortcutDisplay)")
-		attributedTitle.addAttribute(
-			.paragraphStyle,
-			value: paragraphStyle,
-			range: NSRange(location: 0, length: attributedTitle.length)
-		)
-		showItem.attributedTitle = attributedTitle
-
-		let settingsItem = NSMenuItem(title: "Settings...", action: #selector(openSettings), keyEquivalent: ",")
+		let settingsItem = NSMenuItem(title: "Settings...", action: #selector(openSettings), keyEquivalent: "")
 		settingsItem.target = self
 
-		let quitItem = NSMenuItem(title: "Quit Summon", action: #selector(quitApp), keyEquivalent: "q")
+		let extensionsItem = NSMenuItem(title: "Extensions", action: #selector(openExtensions), keyEquivalent: "")
+		extensionsItem.target = self
+
+		let clearClipboardItem = NSMenuItem(title: "Clear Clipboard History", action: #selector(clearClipboard), keyEquivalent: "")
+		clearClipboardItem.target = self
+
+		let quitItem = NSMenuItem(title: "Quit Summon", action: #selector(quitApp), keyEquivalent: "")
 		quitItem.target = self
 
+		menu.addItem(aboutItem)
+		menu.addItem(updateItem)
+		menu.addItem(NSMenuItem.separator())
 		menu.addItem(showItem)
 		menu.addItem(NSMenuItem.separator())
 		menu.addItem(settingsItem)
+		menu.addItem(extensionsItem)
+		menu.addItem(NSMenuItem.separator())
+		menu.addItem(clearClipboardItem)
 		menu.addItem(NSMenuItem.separator())
 		menu.addItem(quitItem)
 
@@ -64,6 +84,32 @@ class StatusBarManager {
 
 	@objc private func openSettings() {
 		onSettings?()
+	}
+
+	@objc private func openExtensions() {
+		onExtensions?()
+	}
+
+	@objc private func clearClipboard() {
+		onClearClipboard?()
+	}
+
+	@objc private func showAbout() {
+		let alert = NSAlert()
+		alert.messageText = "Summon"
+		alert.informativeText = "Version 0.0.1\n\nA fast, efficient launcher for macOS"
+		alert.alertStyle = .informational
+		alert.addButton(withTitle: "OK")
+		alert.runModal()
+	}
+
+	@objc private func checkUpdates() {
+		let alert = NSAlert()
+		alert.messageText = "Check for Updates"
+		alert.informativeText = "Update checking not yet implemented"
+		alert.alertStyle = .informational
+		alert.addButton(withTitle: "OK")
+		alert.runModal()
 	}
 
 	@objc private func quitApp() {
